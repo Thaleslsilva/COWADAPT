0173f633b25555e9bb87ce6afacc9de185f8d10e
Thales de Lima Silva <medvet21@gmail.com>
2026-06-25 18:13:03 -0300

    Modify input files and output names in Porechop.sh
    Updated input files and output names for quality control.

--- pipelines/quality_control/Porechop.sh
+++ pipelines/quality_control/Porechop.sh
@@ -11,16 +11,50 @@
 
 # Input files
 FILES=(
-  "250415_PAW46500_doradov0.9.6_SUP_simplex.fastq"
-  "250415_PBA89745_doradov0.9.6_SUP_simplex.fastq"
-  ...
+  "240514_PAU90126_doradov0.6.1_SUP_simplex.fastq.gz"
+  "240708_PAU90117_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240708_PAW41805_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240708_PAU85751_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240708_PAW41726_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240708_PAW42170_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240708_PAW36795_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240708_PAW41613_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240708_PAW41607_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240807_PAW42124_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240514_PAU89876_doradov0.6.1_SUP_simplex.fastq.gz"
+  "240807_PAW42033_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240807_PAW39366_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240701_PAU85745_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240807_PAW42201_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240710_PAU99997_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240701_PAU87849_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240807_PAW00680_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240710_PAU99920_doradov0.7.2_SUP_simplex.fastq.gz"
+  "240710_PAU99949_doradov0.7.2_SUP_simplex.fastq.gz"
 )
 
 # Output names
 NAMES=(
-  "ONT_001"
-  "ONT_002"
-  ...
+   "COWADAPT_001"
+  "COWADAPT_002"
+  "COWADAPT_003"
+  "COWADAPT_004"
+  "COWADAPT_005"
+  "COWADAPT_006"
+  "COWADAPT_008"
+  "COWADAPT_009"
+  "COWADAPT_010"
+  "COWADAPT_011"
+  "COWADAPT_012"
+  "COWADAPT_013"
+  "COWADAPT_014"
+  "COWADAPT_015"
+  "COWADAPT_016"
+  "COWADAPT_017"
+  "COWADAPT_018"
+  "COWADAPT_019"
+  "COWADAPT_020"
+  "COWADAPT_023"
 )
 
 # Check consistency
